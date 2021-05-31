7c563dc6507ffffa7c8f33419cf31880e1c827ef
suryapampana <17pa5a0320@vishnu.edu.in>
2021-05-31 21:04:26 +0530

    df

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyDynamicInputCompoent />
+      <MyInputCompoent />
     </div>
   );
 }
 
-export default App;
+const MyDynamicInputCompoent = () => {
+  const [list, setList] = useState([]);
+
+  // STEP 1
+  const [tweetInput, setTweetInput] = useState("");
+
+  // STEP 2 :: e is important here;
+  const readAndUpdateTweet = (e) => setTweetInput(e.target.value);
+
+  const postTheTweet = () => {
+    setList([tweetInput, ...list]);
+    setTweetInput("");
+  };
+
+  return (
+    <div>
+      <h2>Tweets</h2>
+      <div>
+        {/** STEP 3 value and onChange */}
+        <input
+          type="text"
+          value={tweetInput}
+          onChange={(e) => readAndUpdateTweet(e)}
+          placeholder="your thought..."
+        />
+        <input
+          type="button"
+          value="Tweet here"
+          onClick={() => postTheTweet()}
+        />
+      </div>
+
+      {list.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </div>
+  );
+};
+
+const MyInputCompoent = () => {
+  const [list, setList] = useState(["Delhi", "Kolkata"]);
+
+  const postTheTweet = () => {
+    setList(["MUMBAI", ...list]);
+  };
+
+  return (
+    <div>
+      <h1>Mini Twitter</h1>
+      <div>
+        <input type="text" value="" placeholder="your thought..." />
+        <input
+          type="button"
+          value="Tweet here"
+          onClick={() => postTheTweet()}
+        />
+      </div>
+
+      <h2>Tweets</h2>
+      {list.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </div>
+  );
+};
