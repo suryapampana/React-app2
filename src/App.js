474fb9b1e72358a784e36ddbf90b65674bc9da60
suryapampana <17pa5a0320@vishnu.edu.in>
2021-05-31 21:57:41 +0530

    mini twitter with delete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
@@ -11,9 +12,20 @@ export default function App() {
     setTextInput("");
   };
 
+  const deleteTweet = (item, index) => {
+    // which tweet to delete
+    list.splice(index, 1);
+
+    // so that we can update the view
+    setList([...list]);
+  };
+
   return (
     <div>
-      <h1 className="bg-primary text-light p-3" style={{ textAlign: "center" }}>
+      <h1
+        className="bg-primary text-light p-3 sticky-top"
+        style={{ textAlign: "center" }}
+      >
         Mini Twitter
       </h1>
 
@@ -34,8 +46,17 @@ export default function App() {
       </div>
 
       {list.map((item, index) => (
-        <div key={index} className="alert alert-secondary">
+        <div
+          key={index}
+          className="alert alert-secondary d-flex justify-content-between align-items-center"
+        >
           {item}
+          <button
+            className="btn btn-outline-danger"
+            onClick={() => deleteTweet(item, index)}
+          >
+            X
+          </button>
         </div>
       ))}
     </div>
